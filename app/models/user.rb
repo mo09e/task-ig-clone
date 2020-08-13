58e4bd48162f9a627e4139f3c40132892cd755f2
Moe <moe_akahane@diveintocode.jp>
2020-08-13 17:51:58 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 30 }
-  validates :nickname, presence: true
+  validates :name,  presence: true, length: { maximum: 20 }
+  validates :nickname, presence: true, length: { maximum: 20}
   validates :image, presence: true
-  validates :birthday, presence: true,length: { is: 8 }
+  validates :birthday, presence: true
   validates :email, presence: true, length: { maximum: 255 },
-                  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
 
   has_secure_password
--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,15 @@ Bundler.require(*Rails.groups)
 module InstagramClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 5.2
-    
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      # g.assets false
+      g.helper false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
