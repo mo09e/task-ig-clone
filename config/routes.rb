62eb73c7c86ed8a61f0d32d80073b45f4c9699cc
Moe <moe_akahane@diveintocode.jp>
2020-08-13 19:38:38 +0900

    セッション機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    resources :users, only: [:new, :create, :show, :update, :edit]
-  end
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show, :update, :edit]
 end
