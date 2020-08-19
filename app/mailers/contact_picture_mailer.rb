f330180fc9be6b1be4112418e81e98036384b9c4
Moe <moe_akahane@diveintocode.jp>
2020-08-19 19:27:27 +0900

    ルーティング、レイアウトを修正

--- app/mailers/contact_picture_mailer.rb
+++ app/mailers/contact_picture_mailer.rb
@@ -1,6 +1,6 @@
 class ContactPictureMailer < ApplicationMailer
   def contact_picture_mail(picture)
     @picture = picture
-    mail to: "moe_akahane@diveintocode.jp", subject: "投稿確認メール"
+    mail to: @picture.user.email, subject: "投稿確認メール"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
 
   # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
+
+  root to: "sessions#new"
 end
