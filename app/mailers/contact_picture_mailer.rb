c0aa3ff127bc2eb1b358816e3d64bb21a99c2a89
Moe <moe_akahane@diveintocode.jp>
2020-08-19 14:54:05 +0900

    不要なコードを削除

--- app/mailers/contact_picture_mailer.rb
+++ app/mailers/contact_picture_mailer.rb
@@ -3,22 +3,4 @@ class ContactPictureMailer < ApplicationMailer
     @picture = picture
     mail to: "moe_akahane@diveintocode.jp", subject: "投稿確認メール"
   end
-
-  # # override
-  # def deliver_later!
-  #   if Rails.env.heroku_review_app?
-  #     deliver_now!
-  #   else
-  #     super
-  #   end
-  # end
-  #
-  #   # override
-  # def deliver_later
-  #   if Rails.env.heroku_review_app?
-  #     deliver_now
-  #   else
-  #     super
-  #   end
-  # end
 end
