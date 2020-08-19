3c5342a6567bc2a399500c8ec3d80d8b1e9ee586
Moe <moe_akahane@diveintocode.jp>
2020-08-19 19:35:49 +0900

    レイアウト、ルーティング調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   end
 
   def choose_new_or_edit
-     if action_name == 'new' || action_name == 'create'
+     if action_name == 'new' || action_name == 'create' || action_name == 'confirm'
        confirm_pictures_path
      elsif action_name == 'edit'
        picture_path
