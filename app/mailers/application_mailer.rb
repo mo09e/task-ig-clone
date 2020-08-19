3dfacbf6e051902b40e5b70342fe6177ada8d7a8
Moe <moe_akahane@diveintocode.jp>
2020-08-19 14:43:36 +0900

    メイラーの設定を本番環境に変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,16 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  # Heroku web apps do not access worker's filesystem
+  # https://github.com/fgrehm/letter_opener_web#usage-on-heroku
+
+  # override
+  def self.method_missing(method_name, *args)
+    if action_methods.include?(method_name.to_s)
+     ContactPictureMailer.new(self, method_name, *args)
+    else
+     super
+   end
+  end
 end
